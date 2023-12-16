54ea2b0ee21ba0263555c5672b79bb483bbc1f75
BlancheK <91051190+BlancheK@users.noreply.github.com>
2023-12-16 22:59:35 +0100

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// assets/js/script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
